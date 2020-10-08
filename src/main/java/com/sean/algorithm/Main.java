95212c6b2d46e4082d92e67374ac8ad6db9f04b7
Sean Yi <1498280793@qq.com>
2020-10-08 22:58:24 +0800

    build base structure

---
+++ src/main/java/com/sean/algorithm/Main.java
@@ -0,0 +1,11 @@
+package com.sean.algorithm;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("This is for Sean practising algorithm");
+    }
+}
