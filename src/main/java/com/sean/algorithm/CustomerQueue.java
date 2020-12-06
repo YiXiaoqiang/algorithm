84ee60a43f0b204249fd6d45bfa09adbb5dd8889
Sean Yi <1498280793@qq.com>
2020-12-06 22:14:00 +0800

    add customer queue

---
+++ src/main/java/com/sean/algorithm/CustomerQueue.java
@@ -0,0 +1,23 @@
+package com.sean.algorithm;
+
+import java.util.Stack;
+
+public class CustomerQueue {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+}
